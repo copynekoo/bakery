3008ec97d1ec066aed20b1227114fb88df2b0647
copynekoo <157344271+copynekoo@users.noreply.github.com>
2025-10-28 06:59:07 +0700

    AllOrders now has customer id column

--- backend/controllers/orderController.js
+++ backend/controllers/orderController.js
@@ -11,7 +11,7 @@ async function getNextOrderId() {
 
 async function getAllOrders() {
   const orders = await sql`
-    select orders.order_id, order_lines.order_method, order_lines.product_id, products.p_name, order_lines.quantity, orders.order_creation_date, orders.status, orders.status_update_date, orders.payment_proof, orders.shipping_destination
+    select orders.order_id, orders.customer_id, order_lines.order_method, order_lines.product_id, products.p_name, order_lines.quantity, orders.order_creation_date, orders.status, orders.status_update_date, orders.payment_proof, orders.shipping_destination
     from orders
     join order_lines
     on orders.order_id = order_lines.order_id
